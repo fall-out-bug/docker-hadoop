42eb7869dd453d152e2f5bdd59f46749a8ff0839
Andrei Zhukov <andrei@beetles.family>
2025-04-27 12:33:55 +0300

    updates

--- hive/run.sh
+++ hive/run.sh
@@ -5,11 +5,13 @@ hdfs dfs -mkdir -p    /user/hive/warehouse
 hdfs dfs -chmod g+w   /tmp
 hdfs dfs -chmod g+w   /user/hive/warehouse
 hdfs dfs -mkdir       /apps
+hdfs dfs -mkdir       /apps/hive
 mkdir /opt/tez_lib
 cp /opt/tez/lib/*.jar /opt/tez_lib
 cp /opt/hive/lib/*.jar /opt/tez_lib
 tar -czvf tez.tar.gz -C /opt/tez_lib .
 hdfs dfs -put tez.tar.gz /apps/
+hdfs dfs -put /opt/hive/lib/*.jar /apps/hive
 
 if [ ! -f /opt/hive/conf/.metastore_initialized ]; then
 echo "Инициализация схемы Hive Metastore..."
